108f5d1d2d30c8f48c8c7b57920f933dcc860546
tirthaa <tirtha@kaashid.com>
2024-06-06 09:50:03 +0000

    from server

--- domains/kaashid.com/public_html/API/RLogOut.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//unset($_SESSION["id"]);
-//unset($_SESSION["name"]);
-
-session_destroy();
-
-header("Location:RLogin.php");
-?>
--- domains/kaashid.com/public_html/index_old.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h2>Hi GemResource</h2>
-
-<h2>Good Morning GemResource</h2>
-</body>
-</html>
