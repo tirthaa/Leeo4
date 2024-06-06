a3494e329ddf0e8dd6ba2ac7575ef8af1169e27c
tirthaa <tirtha@kaashid.com>
2024-06-06 13:34:09 +0000

    First to Server Darsh

--- domains/kaashid.com/public_html/uploadGitHub/uploadFileToGitHub.php
+++ domains/kaashid.com/public_html/uploadGitHub/uploadFileToGitHub.php
@@ -1,39 +1,71 @@
 <?php
 
-$output = `ls -la`;
+$output = `git add --all`;
 echo $output;
 sleep(1);
 
-$output = `git add --all`;
+$output = `git config --global user.email "tirtha@kaashid.com"`;
 echo $output;
 sleep(2);
 
-$output = `git config --global user.email "tirtha@kaashid.com"`;
+$output = `git config --global user.name "tirthaa"`;
 echo $output;
 sleep(3);
 
-$output = `git config --global user.name "tirthaa"`;
+$output = `git commit -m "First to Server Darsh"`;
 echo $output;
 sleep(4);
 
-$output = `git commit -m "First to Server Darsh"`;
+$output = `git remote set-url origin https://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@github.com/tirthaa/darshh.git`;
 echo $output;
 sleep(5);
 
-$output = `git remote set-url origin https://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@github.com/tirthaa/darshh.git`;
+$output = `git remote add github https://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@github.com/tirthaa/darshh.git`;
 echo $output;
 sleep(6);
 
-$output = `git remote add github https://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@github.com/tirthaa/darshh.git`;
+$output = `git config --global  pull.ff true`;
 echo $output;
 sleep(7);
 
-$output = `git config --global  pull.ff true`;
+$output = `git pull origin master --allow-unrelated-histories`;
 echo $output;
 sleep(8);
 
-$output = `git pull origin master --allow-unrelated-histories`;
+$output = `ls -la`;
+echo $output;
+sleep(9);
+
+$output = `git add --all`;
 echo $output;
 sleep(10);
+
+$output = `git config --global user.email "tirtha@kaashid.com"`;
+echo $output;
+sleep(11);
+
+$output = `git config --global user.name "tirthaa"`;
+echo $output;
+sleep(12);
+
+$output = `git commit -m "First to Server Darsh"`;
+echo $output;
+sleep(13);
+
+$output = `git remote set-url origin https://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@github.com/tirthaa/darshh.git`;
+echo $output;
+sleep(14);
+
+$output = `git remote add github https://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@github.com/tirthaa/darshh.git`;
+echo $output;
+sleep(15);
+
+$output = `git config --global  pull.ff true`;
+echo $output;
+sleep(16);
+
+$output = `git push -u origin master`;
+echo $output;
+sleep(17);
 
 ?>
