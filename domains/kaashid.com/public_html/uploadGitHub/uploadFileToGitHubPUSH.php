18e86b8ef2bd92a442bf1fb5f574455c8333042a
tirthaa <tirtha@kaashid.com>
2024-06-07 06:10:08 +0000

    Sixth from Server Darshh

--- domains/kaashid.com/public_html/uploadGitHub/uploadFileToGitHubPUSH.php
+++ domains/kaashid.com/public_html/uploadGitHub/uploadFileToGitHubPUSH.php
@@ -16,11 +16,11 @@ $output = `git commit -m "Nineth to Server Darshh"`;
 echo $output;
 sleep(4);
 
-$output = `git remote set-url origin https://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@github.com/tirthaa/darshhh.git`;
+$output = `git remote set-url origin https://github.com/tirthaa/darshhh.git`;
 echo $output;
 sleep(5);
 
-$output = `git remote add github https://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@github.com/tirthaa/darshhh.git`;
+$output = `git remote add github https://github.com/tirthaa/darshhh.git`;
 echo $output;
 sleep(6);
 
